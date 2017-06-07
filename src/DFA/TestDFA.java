9a7ffba855b895f8fd8ab00884bd317ea8be0d7e
surferbv <surferbv@gmail.com>
2017-06-06 21:12:20 -0700

    Finished DFA and Drone packages

--- src/DFA/TestDFA.java
+++ src/DFA/TestDFA.java
@@ -1,3 +1,24 @@
+/*================================================================================
+* CS544 - Computer Networks
+* Drexel University, Spring 2017
+* Protocol Implementation: Remote Control Drone Protocol
+* Team 4:
+* - Ajinkya Dhage
+* - Ethan Shafer
+* - Brent Varga
+* - Xiaxin Xin
+* --------------------------------------------------------------------------------
+* File name: TesterDFA.java
+*
+* Description:
+* This class provides a testing scenario demonstrate the authentication functionality.
+*
+* Requirements (Additional details can be found in the file below):
+* - CLIENT
+*
+*=================================================================================
+* */
+
 package DFA;
 
 import java.net.Socket;
@@ -15,7 +36,9 @@ import messages.Message.MessageType;
 
 public class TestDFA {
 	public static void main(String[] args) throws Exception {
-		//Steps to be followed for authenticating a receiver hello message and receiver response hello
+		/**
+		 * Steps to be followed for authenticating a receiver hello message and receiver response hello
+		 */
 		byte commandByte = 0x00;
 		JSONObject json = new JSONObject();
 		json.put("password", "pass");
@@ -27,14 +50,18 @@ public class TestDFA {
 		DroneServer droneServer = new DroneServer("8080", "pass", "1234", new UIDrone());
 		DroneDFA droneDFA = new DroneDFA("password", "1.2", "1.23", "2.36");
 		DFAResponse droneResponse = droneDFA.authenticate(message);
-		
-		//Steps to be followed for authenticating a drone hello message
+
+		/**
+		 * Steps to be followed for authenticating a drone hello message
+ 		 */
 		json.put("random number B", "2.3456");
 //		ReceiverClient receiverClient = new ReceiverClient(new Socket(), new UIReceiver(),"pass");
 		ReceiverDFA receiverDFA = new ReceiverDFA("password", "1.2", "1.23");
 		DFAResponse receiverResponse = receiverDFA.authenticate(message);
-		
-		//Steps to be followed for getting next DFA state
+
+		/**
+		 * Steps to be followed for getting next DFA state
+		 */
 		ControlType currentState = ControlType.PREFLIGHT;
 		DFAResponse nextState = DFAState.getNextState(message, currentState);
 	}
--- src/DFA/Utility.java
+++ src/DFA/Utility.java
@@ -1,16 +1,33 @@
+/*================================================================================
+* CS544 - Computer Networks
+* Drexel University, Spring 2017
+* Protocol Implementation: Remote Control Drone Protocol
+* Team 4:
+* - Ajinkya Dhage
+* - Ethan Shafer
+* - Brent Varga
+* - Xiaxin Xin
+* --------------------------------------------------------------------------------
+* File name: Utility.java
+*
+* Description:
+* This class defines the static utility methods that other classes of DFA package will use
+* to perform some basic tasks.
+*
+* Requirements (Additional details can be found in the file below):
+* - NONE
+*
+*=================================================================================
+* */
+
 package DFA;
-/**
- * 
- * @author Ajinkya
- * This class defines the static utility methods that other classes of DFA package will use
- * to perform some basic tasks.
- */
+
 public class Utility {
+
 	/**
-	 * 
+	 * Returns true if field String is null or empty. Otherwise, returns false
 	 * @param field
 	 * @return {@link Boolean}
-	 * Returns true if field String is null or empty. Otherwise, returns false
 	 */
 	static boolean isEmpty(String field){
 		return (field == null || field.length() == 0);
